21539eef90b3decdfbfec736af944d9d991baf93
Srigopinath-A <sgopinath1930@gmail.com>
2025-04-02 06:44:33 +0000

    Error in getting all user

--- Webapp/src/main/java/com/newapp/Webapp/Security/JwtAuthFilter.java
+++ Webapp/src/main/java/com/newapp/Webapp/Security/JwtAuthFilter.java
@@ -30,33 +30,35 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 	// cycle
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+	    throws ServletException, IOException {
 
-		// this is for extracting token from our request
-		// when ever user reaching us with the end point we have get the token and
-		// validate it
-		String token = getTokenFromRequest(request);
-		if (token != null) {
-			String username = jwtutils.getUsernameFromToken(token);
-			UserDetails userdetails = customuserdetailservice.loadUserByUsername(username);
-			if (StringUtils.hasText(username) && jwtutils.isTokenValid(token, userdetails)) {
-				log.info("valid jwt for {}", username);
+	    String token = getTokenFromRequest(request);
+	    if (token != null) {
+	    	String username = jwtutils.getUsernameFromToken(token);
 
-				UsernamePasswordAuthenticationToken authenticationtoken = new UsernamePasswordAuthenticationToken(
-						userdetails, null, userdetails.getAuthorities());
-				authenticationtoken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				
-				SecurityContextHolder.getContext().setAuthentication(authenticationtoken);
-			}
-		}
-		filterChain.doFilter(request, response);
+            UserDetails userDetails = customuserdetailservice.loadUserByUsername(username);
+
+            if (StringUtils.hasText(username) && jwtutils.isTokenValid(token, userDetails)){
+                log.info("VALID JWT FOR {}", username);
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+        }
+        filterChain.doFilter(request, response);
 	}
 
 	private String getTokenFromRequest(HttpServletRequest request) {
 		// first getting token from the header
 		String token = request.getHeader("Authorization");
 		// beare token is used for validation and along we can check it on the post man
-		if (StringUtils.hasText(token) && StringUtils.startsWithIgnoreCase(token, "Bearer ")) {
+		// StringUtils.startsWithIgnoreCase(token, "Bearer ") this is case sensitive 
+		if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
 			return token.substring(7);
 		}
 		return null;
